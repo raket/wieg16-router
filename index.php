f9309ae21a461a28f7f8a2ec177a144fbe116d3c
Marcus Dalgren <marcus@raket.nu>
2017-04-23 23:44:52 +0200

    Basic autoloading

--- index.php
+++ index.php
@@ -1,1 +1,7 @@
 <?php
+spl_autoload_register(function($class) {
+	require 'src/Raket/'.$class.'.php';
+});
+
+$class = new TestClass();
+var_dump($class);
---
+++ src/Raket/TestClass.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: marcusdalgren
+ * Date: 2017-04-23
+ * Time: 23:43
+ */
+class TestClass {
+
+}

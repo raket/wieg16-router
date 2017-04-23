a7ddd8a468fabde005f1cd1fa84345848972c729
Marcus Dalgren <marcus@raket.nu>
2017-04-23 23:10:07 +0200

    Basic router

--- index.php
+++ index.php
@@ -1,1 +1,12 @@
 <?php
+
+function route($path, Callable $response) {
+	$requestedPath = $_SERVER['REQUEST_URI'];
+	if ($requestedPath == $path) {
+		echo $response();
+	}
+}
+
+route('/whatever', function() {
+	return "whatever";
+});
